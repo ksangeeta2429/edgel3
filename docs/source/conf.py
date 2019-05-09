60f929ce2961b4d2edec4635167503317d45c15a
ksangeeta2429 <kumari.14@osu.edu>
2019-05-08 21:09:33 -0400

    doc build fail correction

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Sangeeta Kumari'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 

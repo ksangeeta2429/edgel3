098018edf70d66a6b333deaed6f5c1bed9838a01
romulo <romulomanzano@gmail.com>
2020-05-29 18:36:43 -0400

    Pinning Kapre to 0.1.4

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     install_requires=[
         'numpy>=1.13.0',
         'scipy>=0.19.1',
-        'kapre>=0.1.4',
+        'kapre==0.1.4',
         'keras>=2.0.9',
         'PySoundFile>=0.9.0.post1',
         'resampy>=0.2.1,<0.3.0',

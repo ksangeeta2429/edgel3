a9b68c093cd363f4629c2e380088310655338e28
Sangeeta Srivastava <sangeeta.osu@gmail.com>
2021-04-04 02:06:34 -0400

    removing python 3.5 (deprecated)

--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ setup(
         "Intended Audience :: Science/Research",
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Programming Language :: Python :: 3', #Removing support for python 2.x
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
